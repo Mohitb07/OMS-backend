82ca1f964c8cc899db188cb146d64b3754b2f7a1
Mohitb07 <bmohit980@gmail.com>
2023-03-20 20:33:00 +0530

    refactor: files deleted

--- config/connection.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql2");
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Kuttu@2312",
-  database: "mydata",
-});
-
-module.exports = connection
