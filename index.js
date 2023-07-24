c1292c6f4c8292b63d37a4915c8d2f0b8dc13708
Mohitb07 <bmohit980@gmail.com>
2023-07-24 21:13:28 +0530

    refactor: code refactored

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const PORT = process.env.PORT || 3000;
 const env = process.env.NODE_ENV || "development";
 const corsOptions = corsConfig[env];
 
-console.log("stripe key in index", process.env.STRIPE_SECRET_KEY);
 app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
