321d980f0834112dbc9ea570098f21b575e209cd
Mohitb07 <bmohit980@gmail.com>
2023-08-04 21:25:55 +0530

    fix: cors origin issues

--- config/corsConfig.js
+++ config/corsConfig.js
@@ -1,13 +1,13 @@
 // corsConfig.js
 const devCorsOptions = {
   origin: "*", // Replace with your development frontend URL
-  methods: "GET,PUT,POST,DELETE,PATCH",
+  methods: "GET,PUT,POST,DELETE,PATCH, OPTIONS",
   allowedHeaders: "Content-Type,Authorization",
 };
 
 const prodCorsOptions = {
   origin: process.env.CLIENT_URL, // Replace with your production frontend URL
-  methods: "GET,PUT,POST,DELETE,PATCH",
+  methods: "GET,PUT,POST,DELETE,PATCH, OPTIONS",
   allowedHeaders: "Content-Type,Authorization",
   credentials: true,
 };
