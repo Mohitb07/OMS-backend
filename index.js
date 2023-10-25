5c42b80631d02a39975de6b6a2fc3ab5e6b53cc3
Mohitb07 <bmohit980@gmail.com>
2023-10-25 20:42:10 +0530

    feat: now using different env file based on running environment

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-require("dotenv").config();
+const dotenv = require('dotenv');
+
+if (process.env.NODE_ENV === 'production') {
+  dotenv.config({ path: '.env.production' });
+} else {
+  dotenv.config();
+}
+
 const express = require("express");
 const cors = require("cors");
 const { serve } = require("inngest/express");
