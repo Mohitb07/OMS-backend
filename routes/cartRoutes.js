eb3ed9af4340848387a89fcd20edf998e4123289
mohitb07 <bmohit980@gmail.com>
2024-10-15 11:38:13 +0530

    refactor: improve validation

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const { body } = require("express-validator");
 
 const auth = require("../middleware/auth");
-const cartController = require('../controllers/cartController')
+const cartController = require("../controllers/cartController");
 
 const router = new express.Router();
 
@@ -9,10 +10,32 @@ router.get("/cart", auth, cartController.getCart);
 
 router.get("/cart_items_count", auth, cartController.getCartItemsCount);
 
-router.post("/carts", auth, cartController.addToCart);
+router.post(
+  "/carts",
+  [body("product_id").notEmpty().withMessage("Product ID is required")],
+  auth,
+  cartController.addToCart
+);
 
-router.patch("/carts", auth, cartController.updateCartQuantity);
+router.patch(
+  "/carts",
+  [
+    body("quantity").notEmpty().withMessage("Quantity is required"),
+    body("product_id").notEmpty().withMessage("Product ID is required"),
+    body("cart_id").notEmpty().withMessage("Cart ID is required"),
+  ],
+  auth,
+  cartController.updateCartQuantity
+);
 
-router.delete("/carts", auth, cartController.deleteCartItem);
+router.delete(
+  "/carts",
+  [
+    body("cart_item_id").notEmpty().withMessage("Cart Item ID is required"),
+    body("cart_id").notEmpty().withMessage("Cart ID is required"),
+  ],
+  auth,
+  cartController.deleteCartItem
+);
 
-module.exports = router
+module.exports = router;
