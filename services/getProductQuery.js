49226e4a5f97cee60383db8ca9e3b95b7cf4d6ea
mohitb07 <bmohit980@gmail.com>
2025-03-26 11:53:27 +0530

    feat: implement getProductQuery service for dynamic product searching and sorting

---
+++ services/getProductQuery.js
@@ -0,0 +1,62 @@
+const { Prisma } = require("@prisma/client");
+const { processSearchQuery } = require("./aisearch");
+
+const getProductQuery = async (userQuery, sortby) => {
+  const query = Object.assign({});
+
+  if (userQuery) {
+    const result = await processSearchQuery(userQuery);
+    query["where"] = {
+      OR: [
+        {
+          name: {
+            contains: result.productName ? result.productName : undefined,
+          },
+          price: {
+            gte: result.minPrice ? result.minPrice : undefined,
+            lte: result.maxPrice ? result.maxPrice : undefined,
+          },
+        },
+        {
+          description: {
+            contains: result.productName ? result.productName : undefined,
+          },
+          price: {
+            gte: result.minPrice ? result.minPrice : undefined,
+            lte: result.maxPrice ? result.maxPrice : undefined,
+          },
+        },
+      ],
+    };
+  }
+
+  if (sortby === "price") {
+    query["orderBy"] = {
+      price: Prisma.SortOrder.asc,
+    };
+  }
+
+  if (sortby === "name") {
+    query["orderBy"] = {
+      name: Prisma.SortOrder.asc,
+    };
+  }
+
+  if (sortby === "newest") {
+    query["orderBy"] = {
+      createdAt: Prisma.SortOrder.desc,
+    };
+  }
+
+  if (sortby === "oldest") {
+    query["orderBy"] = {
+      createdAt: Prisma.SortOrder.asc,
+    };
+  }
+
+  return query;
+};
+
+module.exports = {
+  getProductQuery,
+};
