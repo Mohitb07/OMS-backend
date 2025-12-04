e6f162b36ab69638760bc68fac167a5406247a64
mohitb07 <bmohit980@gmail.com>
2025-12-04 13:12:36 +0530

    feat: add get user by id route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,21 @@
 const express = require("express");
-
+const { param } = require("express-validator");
 const auth = require("../middleware/auth");
-const userController = require('../controllers/userController')
+const userController = require("../controllers/userController");
 
 const router = new express.Router();
 
 router.get("/me", auth, userController.userInfo);
+router.get(
+  "/get_user_by_id/:userId",
+  [
+    param("userId")
+      .notEmpty()
+      .withMessage("User Id is required")
+      .isString()
+      .withMessage("User ID must be an integer"),
+  ],
+  userController.getUserById
+);
 
-module.exports = router
+module.exports = router;
