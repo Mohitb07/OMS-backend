9cc35e9115711b6743850c3bb3120c214960b61a
mohitb07 <bmohit980@gmail.com>
2026-08-11 20:40:59 +0530

    fix: same site cookie issue

--- index.js
+++ index.js
@@ -26,6 +26,9 @@ const errorHandler = require("./middleware/globalErrorHandler");
 const prisma = require("./prismaClient");
 
 const app = express();
+if (process.env.NODE_ENV === "production") {
+  app.set("trust proxy", 1);
+}
 const PORT = process.env.PORT || 3000;
 const env = process.env.NODE_ENV || "development";
 const corsOptions = corsConfig[env];
